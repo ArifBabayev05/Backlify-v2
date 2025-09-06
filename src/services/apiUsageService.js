6ea33eb8d5ec29ee99d710afd0190d712cf53e1b
ArifBabayev05 <arifrb@code.edu.az>
2025-09-06 04:45:03 +0400

    Fix user_id field in getApiUsageStats query

--- src/services/apiUsageService.js
+++ src/services/apiUsageService.js
@@ -29,7 +29,7 @@ class ApiUsageService {
       }
       
       if (userId) {
-        query = query.eq('XAuthUserId', userId);
+        query = query.eq('user_id', userId);
       }
       
       const { data, error } = await query;
